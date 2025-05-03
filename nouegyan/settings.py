6d39b653883af724c40741a379e2e03300e309e0
Aditya Kumar <132904608+ErAditya1@users.noreply.github.com>
2025-05-03 21:41:21 +0530

    libraries error fixed

--- nouegyan/settings.py
+++ nouegyan/settings.py
@@ -157,15 +157,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Your custom static assets during development
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')    # Collected static files (for production)
 
-# Media files configuration
+# Media files (User-uploaded content)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Static files storage (WhiteNoise)
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'media')]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
